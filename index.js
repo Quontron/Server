19245d998f11c964c75c8bcfbc7a2da89fe234b8
rquon <rquon.stuent@outlook.com>
2018-05-09 23:38:08 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) => {
-  res.send({bye :'hello world'});
+app.get('/', (req,res)=> {
+  res.send({hi:'there'});
 });
 
 const PORT = process.env.PORT || 5000;
